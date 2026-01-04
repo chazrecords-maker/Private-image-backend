aad585348c90f2b0884a282e45f79e03095effdc
chazrecords-maker <chazrecords@gmail.com>
2026-01-04 15:14:48 -0600

    Delete src/src directory

--- src/src/index.js
+++
@@ -1,8 +0,0 @@
-addEventListener("fetch", event => {
-  event.respondWith(
-    new Response("Worker entry point OK", {
-      status: 200,
-      headers: { "content-type": "text/plain" }
-    })
-  );
-});
